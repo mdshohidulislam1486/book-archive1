78477f6d6488c399f16a4ba169a1465518363f8a
mdshohidulislam1486 <Shohidul Islam>
2021-09-02 02:00:15 +0600

    half done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log(this)
+// featchin data from API 
+const bookSearch = ()=>{
+const search = document.getElementById('search');
+const searchText =search.value
+console.log(searchText)
+const ulr = (`http://openlibrary.org/search.json?q=${searchText}`)
+fetch(ulr)
+.then(res => res.json())
+.then(data => displayBook(data.docs)).catch(error =>{
+    console.log(error)
+    return error
+})
+}
+
+
+
+const displayBook=books=>{
+
+console.log(books.num_found)/* To get this yoiu have to call only data on fetch */
+books?.forEach( author =>{
+console.log(author.author_name[0])
+console.log(author.title)
+console.log(author.publisher[0])
+console.log(author.first_publish_year)
+
+    })
+
+    
+
+
+}
