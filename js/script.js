78b2686e7607eb8b6359d985f71f16f849dc6dce
mdshohidulislam1486 <Shohidul Islam>
2021-09-02 14:18:15 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -3,53 +3,48 @@ const bookSearch = ()=>{
 const search = document.getElementById('search');
 const searchText =search.value
 search.value ='';
+const ulr = (`https://openlibrary.org/search.json?q=${searchText}`);
 
-const ulr = (`https://openlibrary.org/search.json?q=${searchText}`)
-fetch(ulr)
-.then(res => res.json())
-.then(data => displayBook(data.docs)).catch(error =>{
+    fetch(ulr)
+    .then(res => res.json())
+    .then(data =>displayTotalResult(data)).catch(error =>{
     console.log(error)})
 
-fetch(ulr)
-.then(res => res.json())
-.then(data =>displayTotalResult(data)).catch(error =>{
-    console.log(error)
-})
+    fetch(ulr)
+    .then(res => res.json())
+    .then(data => displayBook(data.docs)).catch(error =>{
+    console.log(error)})
 }
 
 const displayTotalResult = total=>{
-const totalResult =document.getElementById('total-result');
-totalResult.textContent ='';
-const div2 = document.createElement('div')
-div2.innerHTML= `
-<h3 class="text-light" class=""><span>Number of result found </span>${total.num_found}</h3>
-`
-totalResult.appendChild(div2)
+    const totalResult =document.getElementById('total-result');
+    totalResult.textContent ='';
+    const div2 = document.createElement('div')
+    div2.innerHTML= `
+    <h3 class="text-light" class=""><span>Number of result found </span><span >${total.num_found}</span></h3> `
+    totalResult.appendChild(div2)
+
 }
 
 
 const displayBook=books=>{
-const bookCard = document.getElementById('disply-card')
-bookCard.textContent ='';
-books.forEach( des =>{
-const div = document.createElement('div')
-div.classList.add('col')
-
-
-div.innerHTML = `
-    <div class="card  h-100">
-        <img src='https://covers.openlibrary.org/b/id/${des.cover_i}-M.jpg' class="card-img-top" alt="">
-     <div class="card-body">
-            <h5 class="card-title">${des.title}</h5>
-            <p><span>Author: </span>${(des.author_name).slice(0,1)}</p>
-            <p><span>Publisher: </span>${(des.publisher).slice(0, 1)}</p>
-            <p><span>Publication year: </span>${des.first_publish_year}</p>
-     </div>
-    </div>
-`
-bookCard.appendChild(div)
-
-
+    const bookCard = document.getElementById('disply-card')
+    bookCard.textContent ='';
+    books.forEach( des =>{
+    const div = document.createElement('div')
+    div.classList.add('col')
+    div.innerHTML = `
+        <div class="card  h-100">
+            <img src='https://covers.openlibrary.org/b/id/${des.cover_i}-M.jpg' class="card-img-top" alt="">
+        <div class="card-body">
+                <h5 class="card-title">${des.title}</h5>
+                <p><span>Author: </span>${des.author_name[0]}</p>
+                <p><span>Publisher: </span>${des.publisher[0]}</p>
+                <p><span>Publication year: </span>${des.first_publish_year}</p>
+        </div>
+        </div>
+    `
+    bookCard.appendChild(div)
     })
 
     
