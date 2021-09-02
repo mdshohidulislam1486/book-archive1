999795edbcf413a5819fd2c9ec630544b500553b
mdshohidulislam1486 <Shohidul Islam>
2021-09-02 22:18:36 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -13,8 +13,26 @@ const ulr = (`https://openlibrary.org/search.json?q=${searchText}`);
     .then(res => res.json())
     .then(data => displayBook(data.docs)).catch(error =>{
     console.log(error)})
+    if(searchText === ''){
+    toggleHiddenSpinner() 
+    }else{
+        toggleSpinner()
+    }
+    
 }
 
+const toggleSpinner=()=>{
+    document.getElementById('spinner').style.display = 'block'
+    document.getElementById('disply-card').style.visibility= 'hidden'
+    // document.getElementById('total-result').style.display ='none'
+}
+const toggleHiddenSpinner=()=>{
+    document.getElementById('spinner').style.display = 'none'
+    document.getElementById('disply-card').style.visibility= 'visible'
+    // document.getElementById('total-result').style.display ='block'
+}
+
+// display total result for the search 
 const displayTotalResult = total=>{
     const search = document.getElementById('search')
     
@@ -25,9 +43,9 @@ const displayTotalResult = total=>{
     <h3 class="text-secondary" class=""><span >${total.num_found}</span> <span> result found for ${search.value}</span></h3> `
     totalResult.appendChild(div2)
     search.value ='';
-
 }
 
+// displaying search book list 
 const displayBook=books=>{
     
         const bookCard = document.getElementById('disply-card')
@@ -41,14 +59,20 @@ const displayBook=books=>{
                 <img id='img-default' src='https://covers.openlibrary.org/b/id/${des.cover_i ?des.cover_i:10909258}-M.jpg' class="card-img-top' alt="">
             <div class="card-body">
                     <h5 class="card-title text-uppercase">${des.title}</h5>
-                    <p><span>Author: </span>${des.author_name.slice(0,1)?des.author_name:"Not found"}</p>
-                    <p><span>Publisher: </span>${des.publisher.slice(0, 1)}</p>
+                    <p><span>Author: </span>${des.author_name?.slice(0,1)}</p>
+                    <p><span>Publisher: </span>${des.publisher?.slice(0, 1)}</p>
                     <p><span>Publication year: </span>${des.first_publish_year}</p>
             </div>
             </div>
         `
         bookCard.appendChild(div)
+        toggleHiddenSpinner();
         
         })
-
+        
 }
+
+/* Note: hi I will appreciate if you can give some suggesions for below questions
+1. How can I use my deafult image from my computer in this situation? I tried tons of things
+2. 
+ */
